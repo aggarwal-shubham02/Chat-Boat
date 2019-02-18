e3e9534167296acd14605009d5a7a6d85c6b51e7
zaid <monis.z.786@gmail.com>
2019-02-18 07:43:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Application Name : Dalmia
+
+Developer Team : Skyline
+Team Members : zaid monis, Shivani Mehrotra, Shubham Aggarwal, Aanchal, Aakash Sinha.
+                
+
+# SpeechText
+Sample test app for SIH
---
+++ app/src/main/java/textspeech/thezaxis/speechtext/MainActivity.java
@@ -0,0 +1,181 @@
+package textspeech.thezaxis.speechtext;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import com.google.gson.JsonElement;
+import java.util.Map;
+
+public class MainActivity extends Activity implements AIListener{
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+
+
+    private Button listenButton;
+    private TextView resultTextView, messageText;
+    private AIService aiService;
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listenButton =findViewById(R.id.listenButton);
+        resultTextView =findViewById(R.id.resultText);
+        messageText = findViewById(R.id.messageText);
+
+
+        final AIConfiguration config = new AIConfiguration("5987e904ba4b4a699b296c548e336fc7",
+                AIConfiguration.SupportedLanguages.DEFAULT,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+
+
+        listenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listenButtonOnClick();
+            }
+        });
+
+
+
+        /*txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+        // hide the action bar
+        //getActionBar().hide();
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });*/
+
+    }
+    public void listenButtonOnClick() {
+        aiService.startListening();
+    }
+
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+
+//                    String str = data.getStringExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    txtSpeechInput.setText(str);
+                }
+                break;
+            }
+
+        }
+    }
+
+    @Override
+    public void onResult(AIResponse response) {
+
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        resultTextView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString);
+        messageText.setText(""+result.getFulfillment().getSpeech());
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+        resultTextView.setText(error.toString());
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+
+}
